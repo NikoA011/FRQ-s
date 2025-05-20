30724a0156310b8014a3e648ca2c1277e7efe3be
NikoA011 <nikoa6@nycstudents.net>
2025-05-20 09:56:44 -0400

    Update partB.java

--- Q1/partB.java
+++ Q1/partB.java
@@ -1,1 +1,12 @@
-
+public int dogWalkerShift(int start, int end) {
+  int money= 0;
+  for (int hour = start; hour <= end; hour++) {
+    if (dogs == maxDogs || (hour >= 9 && hour <= 17)) {
+      money += (dogs*5) + 3;
+    }
+    else{
+      money += dogs * 5
+    }
+  }
+   return money;
+}
