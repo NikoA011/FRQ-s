96bfab8ef0794b68b486f3e0dec73503236d7240
NikoA011 <nikoa6@nycstudents.net>
2025-05-20 09:49:55 -0400

    Update partA.java

--- Q1/partA.java
+++ Q1/partA.java
@@ -1,1 +1,9 @@
+public int walkDogs(int hour){
+  if(maxDogs >= company.numAvailableDogs(hour){
+    company.updateDogs(hour, company.numAvailableDogs(hour);
+    return company.numAvailableDogs(hour);
+  }
+  company.updateDogs(hour, maxDogs);
+  return maxDogs;
 
+}
