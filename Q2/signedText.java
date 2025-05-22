de4d1996aa554802612f900fc3d5b1b609976622
NikoA011 <nikoa6@nycstudents.net>
2025-05-22 09:53:43 -0400

    Update signedText.java

--- Q2/signedText.java
+++ Q2/signedText.java
@@ -1,1 +1,25 @@
+public class SignedText{
+  private String first;
+  private String last;
 
+  public signedText(String first, String last){
+    this.first = first;
+    this.last = last;
+  }
+
+  public String getSigniture(){
+    if (first.equals(null)){
+      return last;
+    }
+    return (first.substring(0,1) + "-" + last);
+  }
+  public String addSigniture(String str){
+    if (str.indexOf(getSigniture()) == 0){
+      return str.substring(getSigniture().length) + getSigniture();
+    }
+    else if (str.indexOf(getSigniture()) == -1){
+      return str + getSigniture();
+    }
+    return str;
+  }
+}
