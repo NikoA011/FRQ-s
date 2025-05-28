a8316f19b610552f1c1399868b785cbcb9ad5cd9
NikoA011 <nikoa6@nycstudents.net>
2025-05-28 09:55:37 -0400

    Update partB.java

--- Q3/partB.java
+++ Q3/partB.java
@@ -1,1 +1,13 @@
-
+public ArrayList<Match> buildMatches(){
+  ArrayList<Match> matches = new ArrayList<Match>();
+  for(int i = 0; i < competitorList.size(); i++){
+    for(int j = competitorList.size()-1; j>i; j++){
+      if(i==j){
+        return matches;
+      }
+      else{
+        matches.add(Match(competitorList.get(i), competitorList.get(j)));
+      }
+    }
+  }
+}
